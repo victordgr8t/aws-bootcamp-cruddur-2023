9d8a0e549678094f80838a7764059345942be24a
Nnamdi Victor Modebe <nnamdimodebe@gmail.com>
2023-03-16 14:46:26 +0000

    updated signin

--- frontend-react-js/src/components/ProfileInfo.js
+++ frontend-react-js/src/components/ProfileInfo.js
@@ -3,7 +3,7 @@ import {ReactComponent as ElipsesIcon} from './svg/elipses.svg';
 import React from "react";
 
 // [TODO] Authenication
-import { Auth } from 'aws-amplify'; 
+import { Auth } from 'aws-amplify';
 
 export default function ProfileInfo(props) {
   const [popped, setPopped] = React.useState(false);
@@ -16,6 +16,7 @@ export default function ProfileInfo(props) {
     try {
         await Auth.signOut({ global: true });
         window.location.href = "/"
+        localStorage.removeItem("access_token")
     } catch (error) {
         console.log('error signing out: ', error);
     }
